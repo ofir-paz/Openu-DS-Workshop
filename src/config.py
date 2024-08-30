864d78eb88e7817bd6ff4e95a36237f0b4b9bb5f
Ofir Paz <ophirpaz2016@gmail.com>
2024-08-30 18:54:55 +0300

    started working on EDA

--- src/config.py
+++ src/config.py
@@ -22,6 +22,8 @@ SRC_PATH = PROJECT_PATH / "src"
 DATA_PATH = PROJECT_PATH / "data"
 TRAIN_IMAGES_PATH = DATA_PATH / "train_images"
 TEST_IMAGES_PATH = DATA_PATH / "test_images"
+TRAIN_SERIES_DESCRIPTIONS_CSV_PATH = DATA_PATH / "train_series_descriptions.csv"
+TRAIN_LABEL_COORDINATES_CSV_PATH = DATA_PATH / "train_label_coordinates.csv"
 TRAIN_CSV_PATH = DATA_PATH / "train.csv"
 TEST_CSV_PATH = DATA_PATH / "test.csv"
 # ======================== End Of Configuration Paths ======================== #
@@ -31,7 +33,7 @@ TEST_CSV_PATH = DATA_PATH / "test.csv"
 EXAMPLE_ID: str = "4003253"
 EXAMPLE_SAGITTAL_T1_ID: str = "1054713880"
 EXAMPLE_AXIAL_T2_ID: str = "2448190387"
-EXAMPLE_STIR_ID: str = "702807833"
+EXAMPLE_SAGITTAL_T2_STIR_ID: str = "702807833"
 # ============================= End Of Constants ============================= #
 
 # Set the root directory of the project.
