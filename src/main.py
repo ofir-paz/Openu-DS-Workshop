e4e965ccfde230d3eea1f37453f3a6455979df4f
Ofir Paz <ophirpaz2016@gmail.com>
2024-09-27 19:40:53 +0300

    set batch_size=1 instead of None (layers accept only batched data)

--- src/main.py
+++ src/main.py
@@ -8,8 +8,8 @@ from src.dataset.spine_dataset import LumbarSpineDataset
 def main() -> None:
     dataset = LumbarSpineDataset(True)
     train_dataset, val_dataset = dataset.split(val_size=0.2)
-    train_dataloader = DataLoader(train_dataset, batch_size=None, shuffle=True, num_workers=8)
-    val_dataloader = DataLoader(val_dataset, batch_size=None, shuffle=False, num_workers=8)
+    train_dataloader = DataLoader(train_dataset, batch_size=1, shuffle=True, num_workers=4)
+    val_dataloader = DataLoader(val_dataset, batch_size=1, shuffle=False, num_workers=4)
 
     model = LumbarSpineStenosisResNet(pretrained=True, progress=True, hidden_size=2048, dropout=0.4,
                                       name="lumbar_spine_cnn_v1")
